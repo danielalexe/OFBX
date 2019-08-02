33955ad01832eb6ae66513bcc1c3d292c0a1fb36
danielalexe <daniel.alexe@outlook.com>
2019-08-02 23:25:34 +0300

    Initial Commit

---
+++ OFBX/OFBX.Android/obj/Debug/90/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 7f5a4bcd772d1089d9a599761fc79101ab9186d1 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1510
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 395621
+	.include	"typemap.mj.inc"
---
+++ OFBX/OFBX/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using OFBX.Models;
+
+namespace OFBX.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
