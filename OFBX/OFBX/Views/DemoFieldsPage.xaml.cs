c5fe2da4cbeb967b0c9ae4d38bdfbd61009f66f9
danielalexe <daniel.alexe@outlook.com>
2019-08-05 12:18:40 +0300

    #8 - OFBX - Demo Menu - Combo Box

--- OFBX/OFBX/Views/DemoFieldsPage.xaml.cs
+++ OFBX/OFBX/Views/DemoFieldsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OFBX.DataObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,18 @@ namespace OFBX.Views
         public DemoFieldsPage()
         {
             InitializeComponent();
+            List<DTOUser> list = new List<DTOUser>();
+            DTOUser dto = new DTOUser();
+            dto.iID_USER = 1;
+            dto.nvUSERNAME = "Demo 1";
+            list.Add(dto);
+            dto = new DTOUser();
+            dto.iID_USER = 2;
+            dto.nvUSERNAME = "Demo 2";
+            list.Add(dto);
+            PickerDemo.ItemsSource = list;
+            PickerDemo.ItemDisplayBinding = new Binding("nvUSERNAME");
+            PickerDemo.SelectedItem = dto;
         }
     }
 }
